54db42a717ca975c688d806eb28df74595fe0c7a
Tripti147 <triptiahmed58@gmail.com>
2023-09-22 23:44:35 +0600

    First Project

---
+++ project1/project1/Controllers/PortfolioController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project1.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            ViewBag.Message = "Education Info.";
+
+            return View();
+        }
+
+        public ActionResult Project()
+        {
+            ViewBag.Message = "Project Info.";
+
+            return View();
+        }
+
+        public ActionResult Personalinfo()
+        {
+            ViewBag.Message = "Personal Info.";
+
+            return View();
+        }
+
+
+    }
+}
