db03d4fe6c1e0c4cb3599ae2c84377ad548d74d3
tripti147 <triptiahmed58@gmail.com>
2023-10-08 23:19:54 +0600

    LabTask.net

---
+++ LabTask.net/LabTask.net/Controllers/RegistrationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTask.net.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Registration ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string name, string userId, string password, string id, string email, DateTime dob)
+        {
+
+            return RedirectToAction("RegistrationSuccess");
+        }
+
+        public ActionResult RegistrationSuccess()
+        {
+            return View();
+        }
+    }
+}
+    
